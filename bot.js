183380b1574665a415c65b20aa6f4d253fd49c64
ukiraven <lastcrow51@gmail.com>
2017-12-07 16:33:14 +0300

    bot1

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 const TOKEN = "465573784:AAGp3Kb2nHHUsahI-0QRHOWkn_MjlVkbrsc";
 
-var bot = new TelegramBot(token, {polling: true});
+var bot = new TelegramBot(TOKEN, {polling: true});
 
 bot.onText(/так блэт/, (msg, match) => {
   const chatId = msg.chat.id;
